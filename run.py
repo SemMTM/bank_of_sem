1919bfabcd3517ff15da336a91181918eb269115
Osama Hendawi <osama.hendawi@hotmail.com>
2024-09-06 16:09:33 +0000

    feat: add log in selection and all api wire ups

--- run.py
+++ run.py
@@ -1,3 +1,40 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('bank_of_sem')
+
+def user_log_in():
+    """
+    Ask user to log in
+    """
+    print("***********************")
+    print("Welcome to Bank of Sem.")
+    print("Da best bank there is.")
+
+    while True:
+        print("***********************\n")
+        print("1. Log In")
+        print("2. Create An Account\n")
+        print("***********************")    
+        option = input("Please select an option (1-2):\n")
+        if option == '1':
+            print("Log in successful!\n") 
+            break
+        elif option == '2':
+            print("Log in successful!\n") 
+            break
+        else: 
+            print("Please select a valid option.\n")
+
+    print(option)
+
+user_log_in()
