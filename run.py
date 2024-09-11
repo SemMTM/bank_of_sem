3e95da254d26a44810cc3600ec43b6b829366c64
Osama Hendawi <osama.hendawi@hotmail.com>
2024-09-11 12:53:23 +0000

    feat: link up username and password validation to create_account() function

--- run.py
+++ run.py
@@ -37,17 +37,30 @@ def user_log_in():
 
 def create_account():
     """
-    Create a new account and upload data to spreadsheet
+    Create a new username and upload data to spreadsheet
     """
-    print("***********************\n")
-    print("Your new username must be unique")
-    new_username = input("Please enter a username:\n")
-    print("Your password must be a 4 digit number")
-    new_password = input("Please enter a password: \n")
-    print("***********************\n")
+    while True:
+        print("***********************\n")
+        print("Your new username must be unique")
+        new_username = input("Please enter a username:\n")
+        
+        if validate_new_username(new_username):
+            break
+
+    print(new_username)
+
+    while True:
+        print("Your password must be a 4 digit number")
+        new_password = input("Please enter a password: \n")
+        print("***********************\n")
+
+        if validate_new_password(new_password):
+            break
+
+    print(new_password)
 
 
-def validate_username(username):
+def validate_new_username(username):
     """
     Validate the user entered username against the database.
     Raises an error if the Username is not unique.
@@ -56,14 +69,14 @@ def validate_username(username):
     existing_username = any(x == username for x in all_usernames)
 
     if existing_username == True:
-        print("Username already exists. Please enter a new username")
+        print("Username already exists. Please enter a new username\n")
         create_account()
     else:
-        print("New username created")
+        print("New username created\n")
         return True
 
 
-def validate_password(password):
+def validate_new_password(password):
     """
     Validates users password. Checks if it is 4 numbers.
     """
@@ -71,13 +84,14 @@ def validate_password(password):
         int(password)
         if len(password) != 4:
             print(f"Invalid password. Password must be 4 numbers, you entered {len(password)}, please try again. \n")
+            return False
     except ValueError:
         print("Invalid password. Password must be 4 numbers, please try again. \n")
+        return False
 
     return True
 
-#def main():
- #   user_log_in()
+def main():
+    user_log_in()
 
-
-validate_password("abs")
+main()
